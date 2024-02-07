3f56db8e77f3ed6679cb6f341037740fe382f8bf
jtristan <trjohnb@amazon.com>
2024-02-07 13:14:37 -0500

    Renaming library

--- SampCert.lean
+++ SampCert.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `Mirror` library.
 -- Import modules here that should be built as part of the library.
-import Mirror.Extraction
+import SampCert.Extraction
--- lakefile.lean
+++ lakefile.lean
@@ -1,12 +1,12 @@
 import Lake
 open Lake DSL
 
-package «mirror» where
+package «sampcert» where
   -- add any package configuration options here
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"1bda3b46774ca7d204e632781b92c31e32828e89"
 
 @[default_target]
-lean_lib «Mirror» where
+lean_lib «SampCert» where
   -- add any library configuration options here
