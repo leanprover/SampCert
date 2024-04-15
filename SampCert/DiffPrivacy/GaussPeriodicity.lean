e93d6371aab27946151ba544a5f6cc092e5a91a2
jtristan <trjohnb@amazon.com>
2024-04-15 12:23:14 -0400

    Proof periodicity

--- SampCert/DiffPrivacy/GaussPeriodicity.lean
+++ SampCert/DiffPrivacy/GaussPeriodicity.lean
@@ -7,6 +7,32 @@ Authors: Jean-Baptiste Tristan
 import SampCert.DiffPrivacy.GaussBound
 import SampCert.DiffPrivacy.GaussConvergence
 
+theorem SGShift (μ ss : ℝ) (n : ℕ) (k : ℤ) :
+  sg' ss μ (((n + k) : ℤ) : ℝ) = sg' ss (μ - k) n := by
+  simp [sg']
+  ring_nf
+
+theorem SGShift' (μ ss : ℝ) (n : ℕ) (k : ℤ) :
+  sg' ss μ (((-(n + k)) : ℤ) : ℝ) = sg' ss (-(μ + k)) n := by
+  simp [sg']
+  ring_nf
+
+theorem SGSummableShift (μ ss : ℝ) (h : ss > 0) (k : ℤ) :
+  Summable fun (n : ℕ) => (sg' ss μ) ((n + k) : ℤ) := by
+  conv =>
+    right
+    intro n
+    rw [SGShift μ ss n k]
+  apply GaussConvergenceNatPos _ _ h
+
+theorem SGSummableShift' (μ ss : ℝ) (h : ss > 0) (k : ℤ) :
+  Summable fun (n : ℕ) => (sg' ss μ) ((-(n + k)) : ℤ) := by
+  conv =>
+    right
+    intro n
+    rw [SGShift' μ ss n k]
+  apply GaussConvergenceNatPos _ _ h
+
 theorem SG_1_periodic (ss μ : ℝ) (h : ss > 0) :
   (∑' (n : ℤ), sg' ss μ n) = ∑' (n : ℤ), sg' ss (μ + 1) n := by
   have A : ∀ n : ℤ, sg' ss (μ + 1) n = sg' ss μ (n - 1) := by
@@ -15,14 +41,27 @@ theorem SG_1_periodic (ss μ : ℝ) (h : ss > 0) :
   conv => enter [2,1, n] ; rw [A]
   clear A
 
-  have S1 : Summable fun (n : ℕ) => (sg' ss μ) ((n + 1) : ℤ) := sorry
-  have S2 : Summable fun (n : ℕ) => (sg' ss μ) ((-(n + 1)) : ℤ) := sorry
+  have S1 : Summable fun (n : ℕ) => (sg' ss μ) ((n + 1) : ℤ) := by
+    apply SGSummableShift _ _ h
+  have S2 : Summable fun (n : ℕ) => (sg' ss μ) ((-(n + 1)) : ℤ) := by
+    apply GaussConvergenceNatNeg μ ss h
   have X := @tsum_of_add_one_of_neg_add_one ℝ _ _ _ _ (fun (n : ℤ) => (sg' ss μ) n) S1 S2
   rw [X]
   clear X
 
-  have S3 : Summable fun (n : ℕ) => (sg' ss μ) (((n + 1) : ℤ) - 1) := sorry
-  have S4 : Summable fun (n : ℕ) => (sg' ss μ) ((((-(n + 1)) : ℤ) - 1)) := sorry
+  have S3 : Summable fun (n : ℕ) => (sg' ss μ) (((n + 1) : ℤ) - 1) := by
+    simp
+    apply GaussConvergenceNatPos μ ss h
+  have S4 : Summable fun (n : ℕ) => (sg' ss μ) ((((-(n + 1)) : ℤ) - 1)) := by
+    have X : ∀ n : ℕ, ((-(n + 1)) : ℤ) - (1 : ℝ) = ((-(n + 2)) : ℤ) := by
+      intro n
+      simp
+      ring_nf
+    conv =>
+      right
+      intro n
+      rw [X]
+    apply SGSummableShift' μ ss h
   have Y := @tsum_of_add_one_of_neg_add_one ℝ _ _ _ _ (fun (n : ℤ) => (sg' ss μ) (n - 1)) S3 S4
   rw [Y]
   clear Y
@@ -32,11 +71,16 @@ theorem SG_1_periodic (ss μ : ℝ) (h : ss > 0) :
   clear S1 S2 S3 S4
 
   have S5 : Summable fun (n : ℕ) => sg' ss μ (n : ℤ) := by
-    apply GaussConvergence' μ ss h
+    apply GaussConvergenceNatPos μ ss h
   have X5 := @tsum_eq_zero_add ℝ _ _ _ _ (fun (n : ℕ) => (sg' ss μ) n) S5
   rw [X5]; clear X5 S5
 
-  have S6 :  Summable fun (n : ℕ) => sg' ss μ (n + 1) := sorry
+  have S6 :  Summable fun (n : ℕ) => sg' ss μ (n + 1) := by
+    have S := @SGSummableShift μ ss h 1
+    apply Summable.congr S
+    intro b
+    simp
+
   have X6 := @tsum_eq_zero_add ℝ _ _ _ _ (fun (n : ℕ) => (sg' ss μ) (n + 1)) S6
   rw [X6]
 
@@ -44,7 +88,18 @@ theorem SG_1_periodic (ss μ : ℝ) (h : ss > 0) :
 
   rw [X6] ; clear X6 S6
 
-  have S7 : Summable fun (n : ℕ) => sg' ss μ (-1 + -(@Nat.cast ℝ AddMonoidWithOne.toNatCast n)) := sorry
+  have S7 : Summable fun (n : ℕ) => sg' ss μ (-1 + -(@Nat.cast ℝ AddMonoidWithOne.toNatCast n)) := by
+    have X : ∀ n : ℕ, -(1 : ℝ) + -n = -(n + 1) := by
+      simp
+    conv =>
+      right
+      intro n
+      rw [X]
+    have S := SGSummableShift' μ ss h 1
+    apply Summable.congr S
+    intro b
+    simp
+
   have X7 := @tsum_eq_zero_add ℝ _ _ _ _ (fun (n : ℕ) => (sg' ss μ) (-1 + -(@Nat.cast ℝ AddMonoidWithOne.toNatCast n ))) S7
   rw [X7] ; clear X7 S7
 
