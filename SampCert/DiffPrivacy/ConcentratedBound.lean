0987d4a63f6adabb08969d6a4849a6b4ca030d65
jtristan <trjohnb@amazon.com>
2024-04-16 07:40:16 -0400

    Proved some side conditions for prop 5

--- SampCert/DiffPrivacy/ConcentratedBound.lean
+++ SampCert/DiffPrivacy/ConcentratedBound.lean
@@ -13,6 +13,16 @@ open Real
 noncomputable def RenyiDivergence (p q : ℝ → ℝ) (α : ℝ) : ℝ :=
   (1 / (α - 1)) * Real.log (∑' x : ℤ, (p x)^α  * (q x)^(1 - α))
 
+theorem sg_sum_pos (μ : ℤ) (ss : ℝ) (h : ss > 0) :
+  0 < (∑' (x : ℤ), sg' ss μ x) := by
+  unfold sg'
+  apply tsum_pos (GaussConvergence _ _ h) _ 0
+  . apply exp_pos
+  . intro i
+    apply exp_nonneg
+
+-- rw [inv_pos] to get result for inverse
+
 theorem RenyiDivergenceBound (μ : ℤ) (ss : ℝ) (h : ss > 0) (h' : α > 1) :
   RenyiDivergence (fun (x : ℝ) => sg' ss μ x / ∑' x : ℤ, sg' ss μ x)
                   (fun (x : ℝ) => sg' ss (0 : ℤ) x / ∑' x : ℤ, sg' ss (0 : ℤ) x)
@@ -23,11 +33,32 @@ theorem RenyiDivergenceBound (μ : ℤ) (ss : ℝ) (h : ss > 0) (h' : α > 1) :
   rw [← le_div_iff' A]
   refine Real.exp_le_exp.mp ?_
   rw [exp_log]
-  have B : ∀ μ : ℤ, ∀ x : ℝ, 0 ≤ sg' ss μ x := sorry
-  have B' : ∀ x : ℝ, 0 ≤ sg' ss 0 x := sorry
-  have C : ∀ μ : ℤ, 0 ≤ (∑' (x : ℤ), sg' ss μ x)⁻¹ := sorry
-  have C' : 0 ≤ (∑' (x : ℤ), sg' ss 0 x)⁻¹ := sorry
-  have D : 0 < (∑' (x : ℤ), sg' ss 0 x)⁻¹ := sorry
+  have B : ∀ μ : ℤ, ∀ x : ℝ, 0 ≤ sg' ss μ x := by
+    intro μ x
+    unfold sg'
+    apply exp_nonneg
+  have B' : ∀ x : ℝ, 0 ≤ sg' ss 0 x := by
+    unfold sg'
+    intro x
+    apply exp_nonneg
+  have C : ∀ μ : ℤ, 0 ≤ (∑' (x : ℤ), sg' ss μ x)⁻¹ := by
+    intro μ
+    rw [inv_nonneg]
+    apply le_of_lt
+    apply sg_sum_pos _ _ h
+  have C' : 0 ≤ (∑' (x : ℤ), sg' ss 0 x)⁻¹ := by
+    rw [inv_nonneg]
+    apply le_of_lt
+    conv =>
+      right
+      rw [← Int.cast_zero]
+    apply sg_sum_pos _ _ h
+  have D : 0 < (∑' (x : ℤ), sg' ss 0 x)⁻¹ := by
+    rw [inv_pos]
+    conv =>
+      right
+      rw [← Int.cast_zero]
+    apply sg_sum_pos _ _ h
   . -- First, I work on the denominator
     conv =>
       left
@@ -106,7 +137,11 @@ theorem RenyiDivergenceBound (μ : ℤ) (ss : ℝ) (h : ss > 0) (h' : α > 1) :
     unfold sg'
     unfold sg' at F
     clear A B B' C C' D X E
-    have X : 0 < ∑' (x : ℤ), rexp (-(↑x - 0) ^ 2 / (2 * ss)) := sorry
+    have X : 0 < ∑' (x : ℤ), rexp (-(↑x - 0) ^ 2 / (2 * ss)) := by
+      conv =>
+        right
+        rw [← Int.cast_zero]
+      apply sg_sum_pos _ _ h
     have G := @div_le_one ℝ _ (∑' (x : ℤ), rexp (-(↑x - α * ↑μ) ^ 2 / (2 * ss))) (∑' (x : ℤ), rexp (-(↑x - 0) ^ 2 / (2 * ss)))
     replace G := (G X).2 F
     clear X F
@@ -119,6 +154,6 @@ theorem RenyiDivergenceBound (μ : ℤ) (ss : ℝ) (h : ss > 0) (h' : α > 1) :
     conv =>
       right
       rw [← mul_div_assoc]
-    sorry
-    --rw [mul_le_of_le_one_right]
+    apply mul_le_of_le_one_right _ G
+    apply exp_nonneg
   . sorry
