1db22931dd6890f0ce6fa26c9074ea9c56a96203
de Medeiros <markusjd@amazon.com>
2024-05-24 14:06:56 -0400

    document zcdp defs

--- SampCert/DifferentialPrivacy/ZeroConcentrated/DP.lean
+++ SampCert/DifferentialPrivacy/ZeroConcentrated/DP.lean
@@ -3,7 +3,6 @@ Copyright (c) 2024 Amazon.com, Inc. or its affiliates. All Rights Reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jean-Baptiste Tristan
 -/
-
 import Mathlib.Topology.Algebra.InfiniteSum.Basic
 import Mathlib.Topology.Algebra.InfiniteSum.Ring
 import Mathlib.Algebra.Group.Basic
@@ -18,19 +17,41 @@ import Mathlib.Probability.ProbabilityMassFunction.Integrals
 import Mathlib.Analysis.Convex.SpecificFunctions.Basic
 import Mathlib.Analysis.Convex.Integral
 
+/-!
+# Zero Concentrated Differential Privacy
+
+This file defines zero concentrated differential privacy (zCDP).
+-/
+
+
+/--
+Definition of zCDP in terms of Renyi Divergence between neighbouring databases.
+-/
 def DP (q : List T → SLang U) (ε : ℝ) : Prop :=
   ∀ α : ℝ, 1 < α → ∀ l₁ l₂ : List T, Neighbour l₁ l₂ →
   RenyiDivergence (q l₁) (q l₂) α ≤ (1/2) * ε ^ 2 * α
 
-def NonZeroNQ (nq : List T → SLang U) :=
+/--
+No element in the output of a mechanism occurs with zero probability.
+-/
+def NonZeroNQ (nq : List T → SLang U) : Prop :=
   ∀ l : List T, ∀ n : U, nq l n ≠ 0
 
-def NonTopSum (nq : List T → SLang U) :=
+/--
+The output distribution of a mechanism has a finite sum.
+-/
+def NonTopSum (nq : List T → SLang U) : Prop :=
   ∀ l : List T, ∑' n : U, nq l n ≠ ⊤
 
-def NonTopNQ (nq : List T → SLang U) :=
+/--
+Each value in the output distribution of a mechanism is finite.
+-/
+def NonTopNQ (nq : List T → SLang U) : Prop :=
   ∀ l : List T, ∀ n : U, nq l n ≠ ⊤
 
+/--
+The Renyi divergence between neighbouring elements of the output of ``nq`` is finite.
+-/
 def NonTopRDNQ (nq : List T → SLang U) : Prop :=
   ∀ α : ℝ, 1 < α → ∀ l₁ l₂ : List T, Neighbour l₁ l₂ →
   ∑' (x : U), nq l₁ x ^ α * nq l₂ x ^ (1 - α) ≠ ⊤
@@ -39,6 +60,10 @@ def NonTopRDNQ (nq : List T → SLang U) : Prop :=
 --   ∀ α : ℝ, 1 < α → ∀ l₁ l₂ : List T, Neighbour l₁ l₂ →
 --   ∑' (x : U), nq l₁ x ^ α * nq l₂ x ^ (1 - α) ≠ 0
 
+
+/--
+Zero-concentrated differential privacy
+-/
 def zCDP (q : List T → SLang U) (ε : ℝ) : Prop :=
     DP q ε
   ∧ NonZeroNQ q
