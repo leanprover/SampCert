012a6e474bf64d463ee8848cae3ec81319df0bbd
de Medeiros <markusjd@amazon.com>
2024-05-30 11:12:45 -0400

    wip loop spec

--- SampCert/Extractor/Abstract.lean
+++ SampCert/Extractor/Abstract.lean
@@ -4,36 +4,48 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jean-Baptiste Tristan
 -/
 
--- Is this correctly bundled? Using this structure seems challenging. Feels like the monad should be in place of T.
-class Capsid (T : Type) where
-  CapsM : Type → Type
-  CapsM_inst : Monad CapsM
+import Mathlib.Order.SetNotation
 
-  capsWhile : (T → Bool) → (T → CapsM T) → (init : T) → CapsM T
+class Capsid (CapsM : Type u -> Type v) extends Monad CapsM where
+  capsWhile : (cond : T → Bool) → (body : T → CapsM T) → T → CapsM T
 
-def ret_false_test [C : Capsid T] : C.CapsM Bool := C.CapsM_inst.pure False
+section capsid_wf
+  variable {CapsM : Type u -> Type v} [C : Capsid CapsM]
+  variable {T : Type u}
+  variable (cond : T -> Bool) (body : T -> CapsM T)
 
-class Capsid_semibundled (CapsM : Type u -> Type v) extends Monad CapsM where
-  capsWhile : (T → Bool) → (T → CapsM T) → (init : T) → CapsM T
+  open Capsid
 
-def ret_false_test' [Capsid_semibundled CapsM] : CapsM Bool := return false
+  def capsIter (fuel : Nat) : T -> (OptionT CapsM) T
+    := fun t =>
+       match fuel with
+       | Nat.zero => failure
+       | Nat.succ fuel' =>
+          if cond t
+            then (body t) >>= (capsIter fuel')
+            else pure t
 
+  def capsIterPartial (fuel : Nat) : T -> CapsM T
+    := fun t =>
+       match fuel with
+       | Nat.zero => return t
+       | Nat.succ fuel' =>
+          if cond t
+            then (body t) >>= (capsIterPartial fuel')
+            else pure t
 
+  -- Partial correctness specification for While
+  -- Since we're going to translate it into a while loop, we want specify that the shallow embedding to behave like a
+  -- the limit of loop iterations.
+  def CapsWhileSpec : Prop
+    := ∀ t0, capsWhile cond body t0 = sorry
+--         ∀ t0 : T, ∃ i : Nat, ((∀ j : Nat, j < i -> iter body cond j t0 = failure) ∧ ¬ (iter body cond i t0 = failure))
+--     -> iter body cond i t0 = loop T body cond t0
+--     Err... This is not right. Maybe we require that the well-formed Capsid instances are topological types?
+--     Do we want to say that "terminating executions of loops equal a finite unrolling" or "loops are the
+--     limit of finite unrollings"?
 
+end capsid_wf
 
-
-
-/-
-Finite unrolling of a loop with a signal for max depth reached
--/
-
-
-
-
--- def iter [Monad m] (body : T -> m T) (cond : T -> Bool) (fuel : Nat) (t : T) : (OptionT m) T
---   := match fuel with
---      | Nat.zero => failure
---      | Nat.succ fuel' =>
---         if cond t
---           then (body t) >>= (iter body cond fuel')
---           else pure t
+class CapsidWF (CapsM : Type u -> Type v) extends Capsid CapsM where
+  capsWhileWF : ∀ (cond : T -> Bool), ∀ (body : T -> CapsM T), CapsWhileSpec cond body
