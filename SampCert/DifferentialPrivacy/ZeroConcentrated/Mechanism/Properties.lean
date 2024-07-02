481130e67b12950770c159996a5988f1b6bc290e
de Medeiros <markusjd@amazon.com>
2024-07-02 15:03:32 -0400

    progress on mechanism

--- SampCert/DifferentialPrivacy/ZeroConcentrated/Mechanism/Properties.lean
+++ SampCert/DifferentialPrivacy/ZeroConcentrated/Mechanism/Properties.lean
@@ -18,6 +18,9 @@ open Classical Nat Int Real ENNReal MeasureTheory Measure
 
 namespace SLang
 
+set_option pp.coercions false
+-- set_option pp.all true
+
 /--
 The zCDP mechanism with bounded sensitivity satisfies the bound for ``(Δε₂/ε₁)^2``-zCDP.
 -/
@@ -29,157 +32,209 @@ theorem privNoisedQuery_zCDPBound (query : List T → ℤ) (Δ ε₁ ε₂ : ℕ
   apply le_trans A
   clear A
 
-  -- Pull out the ENNReal.ofReal, reducing it to a Real case
-  rw [ENNReal.div_eq_inv_mul]
-  have L : (OfNat.ofNat 2 * ((Δ * ε₂).val.cast / ε₁.val.cast) ^ OfNat.ofNat 2)⁻¹ = ENNReal.ofReal ((OfNat.ofNat 2 * ((Δ * ε₂).val.cast / ε₁.val.cast) ^ OfNat.ofNat 2)⁻¹ ) := by
-    simp
-    rw [← div_pow]
-    conv =>
-      rhs
-      arg 1
-      rw [mul_comm]
-    rw [ENNReal.mul_inv]
-    · rw [ENNReal.ofReal_mul ?G1]
-      case G1 =>
+  -- Turn it into an equality ASAP
+  rw [sensitivity] at bounded_sensitivity
+  have bounded_sensitivity := bounded_sensitivity l₁ l₂ h2
+  have X :
+    (ENNReal.ofReal α *
+      (ENNReal.ofReal ((query l₁ - query l₂).cast ^ OfNat.ofNat 2) /
+        (OfNat.ofNat 2 * (ofNNReal (NNReal.ofPNat (Δ * ε₂)) / ofNNReal (NNReal.ofPNat ε₁)) ^ OfNat.ofNat 2))) ≤
+  (ENNReal.ofReal α *
+      (ENNReal.ofReal (Δ ^ OfNat.ofNat 2) /
+        (OfNat.ofNat 2 * (ofNNReal (NNReal.ofPNat (Δ * ε₂)) / ofNNReal (NNReal.ofPNat ε₁)) ^ OfNat.ofNat 2))) := by
+      refine (ENNReal.mul_le_mul_left ?G1 ?G2).mpr ?G3
+      case G1 => sorry
+      case G2 => sorry
+      refine ENNReal.div_le_div ?G4 ?G5
+      case G5 => rfl
+      apply ofReal_le_ofReal
+      refine sq_le_sq.mpr ?G4.h.a
+      simp only [NNReal.ofPNat, Nonneg.mk_natCast]
+      rw [NNReal.abs_eq]
+      have X1 : NNReal.toReal ((query l₁ - query l₂).natAbs).cast ≤ NNReal.toReal Δ.val.cast := by
         simp
-      congr
-      · rw [ENNReal.ofReal_inv_of_pos]
-        congr
-        simp
-        exact zero_lt_two
-      · rw [ENNReal.inv_pow]
-        rw [ENNReal.ofReal_pow ?G]
-        case G =>
-          apply div_nonneg
-          · simp
-          · simp
-        congr
-        repeat rewrite [ENNReal.div_eq_inv_mul]
-        rw [ENNReal.mul_inv ?G1 ?G2]
-        case G1 =>
-          left
-          simp
-        case G2 =>
-          left
-          simp
-        simp
-        rw [div_eq_mul_inv]
-        rw [ENNReal.ofReal_mul]
-        · simp
-          congr
-          rw [ENNReal.mul_inv]
-          · rw [ENNReal.ofReal_mul]
-            · rw [mul_comm]
-              congr
-              · rw [ENNReal.ofReal_inv_of_pos]
-                · congr
-                  simp
-                · simp
-              · rw [ENNReal.ofReal_inv_of_pos]
-                · congr
-                  simp
-                · simp
-            · simp
-          · left ; simp
-          · left ; simp
-        · simp
-    · left ; simp
-    · left ; simp
-  sorry
-  /-
-  rw [L]
-  have H1 : OfNat.ofNat 0 ≤ ((OfNat.ofNat (OfNat.ofNat 2) * ((Δ * ε₂).val.cast / ε₁.val.cast) ^ OfNat.ofNat (OfNat.ofNat 2))⁻¹ : ℝ) := by
-    simp
-    apply div_nonneg
-    · simp
-    · simp
-  have H2 : OfNat.ofNat 0 ≤ α := by linarith
-  conv =>
-    lhs
-    arg 2
-    rw [<- ENNReal.ofReal_mul]
-    · skip
-    . apply H1
-  conv =>
-    lhs
-    rw [<- ENNReal.ofReal_mul]
-    · skip
-    · apply H2
-  clear H1
-  clear H2
-  apply ofReal_le_ofReal_iff'.mpr
-
-
-  replace bounded_sensitivity := bounded_sensitivity l₁ l₂ h2
-  ring_nf
-  simp
-  conv =>
-    left
-    left
-    left
-    rw [mul_pow]
-  conv =>
-    left
-    rw [mul_assoc]
-    right
-    rw [mul_comm]
-    rw [← mul_assoc]
-
-  left
-  -- Move α to left
-  conv =>
-    rhs
-    rw [mul_assoc]
-    rw [mul_comm]
-  -- Move ε₁^2 to the left
-  conv =>
-    lhs
-    rw [<- mul_assoc]
-    arg 1
-    rw [mul_assoc]
-    arg 2
-    rw [mul_comm]
-  conv =>
-    lhs
-    rw [<- mul_assoc]
-    rw [<- mul_assoc]
-  -- Add factor of 1 on the right
-  conv =>
-    rhs
-    rw [<- (mul_one (α * ε₁ ^ 2 * ((ε₂ : ℝ)^ 2)⁻¹ : ℝ))]
-  -- Reassocaite on left
-  conv =>
-    lhs
-    rw [mul_assoc]
-  apply mul_le_mul
-  · simp only [inv_pow, le_refl]
-  · simp
-    rw [inv_mul_eq_div]
-    rw [div_le_one_iff]
-    left
-    apply And.intro
-    · apply sq_pos_iff.mpr
-      simp
-    · apply sq_le_sq.mpr
-      clear L h2 h1 α
-      rw [<- Int.dist_eq]
-      rw [Int.dist_eq']
-      have X :  ((query l₁ - query l₂).natAbs : ℝ) ≤ (Δ.val : ℝ) := by
-        apply Nat.cast_le.mpr
         apply bounded_sensitivity
-      rw [abs_eq_natAbs]
-      simp only [abs_cast]
-      exact X
-  · apply mul_nonneg
-    · apply sq_nonneg
-    · apply sq_nonneg
-  · apply mul_nonneg
-    · apply mul_nonneg
-      · linarith
-      · apply sq_nonneg
-    · apply inv_nonneg.mpr
-      apply sq_nonneg
-  -/
+      apply (le_trans _ X1)
+      clear X1
+      rw [Int.cast_sub]
+      rw [NNReal.coe_natCast]
+      sorry
+  apply le_trans
+  · apply X
+  clear X
+  apply Eq.le
+  rw [ENNReal.ofReal_mul ?G1]
+  case G1 => sorry
+  rw [ENNReal.ofReal_mul ?G1]
+  case G1 => simp
+  rw [mul_comm]
+  congr
+  repeat rw [division_def]
+
+  -- Doable, if annoying
+  sorry
+
+  -- -- Pull out the ENNReal.ofReal, reducing it to a Real case
+  -- rw [ENNReal.div_eq_inv_mul]
+  -- simp
+  -- simp [ofNNReal]
+  -- rw [ENNReal.ofReal_mul ?G1]
+  -- case G1 => sorry
+  -- rw [ENNReal.ofReal_mul ?G1]
+  -- case G1 => simp
+  -- rw [mul_comm]
+
+  -- skip
+
+  -- have L : (OfNat.ofNat 2 * ((Δ * ε₂).val.cast / ε₁.val.cast) ^ OfNat.ofNat 2)⁻¹ = ENNReal.ofReal ((OfNat.ofNat 2 * ((Δ * ε₂).val.cast / ε₁.val.cast) ^ OfNat.ofNat 2)⁻¹ ) := by
+  --   simp
+  --   rw [← div_pow]
+  --   conv =>
+  --     rhs
+  --     arg 1
+  --     rw [mul_comm]
+  --   rw [ENNReal.mul_inv]
+  --   · rw [ENNReal.ofReal_mul ?G1]
+  --     case G1 =>
+  --       simp
+  --     congr
+  --     · rw [ENNReal.ofReal_inv_of_pos]
+  --       congr
+  --       simp
+  --       exact zero_lt_two
+  --     · rw [ENNReal.inv_pow]
+  --       rw [ENNReal.ofReal_pow ?G]
+  --       case G =>
+  --         apply div_nonneg
+  --         · simp
+  --         · simp
+  --       congr
+  --       repeat rewrite [ENNReal.div_eq_inv_mul]
+  --       rw [ENNReal.mul_inv ?G1 ?G2]
+  --       case G1 =>
+  --         left
+  --         simp
+  --       case G2 =>
+  --         left
+  --         simp
+  --       simp
+  --       rw [div_eq_mul_inv]
+  --       rw [ENNReal.ofReal_mul]
+  --       · simp
+  --         congr
+  --         rw [ENNReal.mul_inv]
+  --         · rw [ENNReal.ofReal_mul]
+  --           · rw [mul_comm]
+  --             congr
+  --             · rw [ENNReal.ofReal_inv_of_pos]
+  --               · congr
+  --                 simp
+  --               · simp
+  --             · rw [ENNReal.ofReal_inv_of_pos]
+  --               · congr
+  --                 simp
+  --               · simp
+  --           · simp
+  --         · left ; simp
+  --         · left ; simp
+  --       · simp
+  --   · left ; simp
+  --   · left ; simp
+  -- sorry
+  -- /-
+  -- rw [L]
+  -- have H1 : OfNat.ofNat 0 ≤ ((OfNat.ofNat (OfNat.ofNat 2) * ((Δ * ε₂).val.cast / ε₁.val.cast) ^ OfNat.ofNat (OfNat.ofNat 2))⁻¹ : ℝ) := by
+  --   simp
+  --   apply div_nonneg
+  --   · simp
+  --   · simp
+  -- have H2 : OfNat.ofNat 0 ≤ α := by linarith
+  -- conv =>
+  --   lhs
+  --   arg 2
+  --   rw [<- ENNReal.ofReal_mul]
+  --   · skip
+  --   . apply H1
+  -- conv =>
+  --   lhs
+  --   rw [<- ENNReal.ofReal_mul]
+  --   · skip
+  --   · apply H2
+  -- clear H1
+  -- clear H2
+  -- apply ofReal_le_ofReal_iff'.mpr
+
+
+  -- replace bounded_sensitivity := bounded_sensitivity l₁ l₂ h2
+  -- ring_nf
+  -- simp
+  -- conv =>
+  --   left
+  --   left
+  --   left
+  --   rw [mul_pow]
+  -- conv =>
+  --   left
+  --   rw [mul_assoc]
+  --   right
+  --   rw [mul_comm]
+  --   rw [← mul_assoc]
+
+  -- left
+  -- -- Move α to left
+  -- conv =>
+  --   rhs
+  --   rw [mul_assoc]
+  --   rw [mul_comm]
+  -- -- Move ε₁^2 to the left
+  -- conv =>
+  --   lhs
+  --   rw [<- mul_assoc]
+  --   arg 1
+  --   rw [mul_assoc]
+  --   arg 2
+  --   rw [mul_comm]
+  -- conv =>
+  --   lhs
+  --   rw [<- mul_assoc]
+  --   rw [<- mul_assoc]
+  -- -- Add factor of 1 on the right
+  -- conv =>
+  --   rhs
+  --   rw [<- (mul_one (α * ε₁ ^ 2 * ((ε₂ : ℝ)^ 2)⁻¹ : ℝ))]
+  -- -- Reassocaite on left
+  -- conv =>
+  --   lhs
+  --   rw [mul_assoc]
+  -- apply mul_le_mul
+  -- · simp only [inv_pow, le_refl]
+  -- · simp
+  --   rw [inv_mul_eq_div]
+  --   rw [div_le_one_iff]
+  --   left
+  --   apply And.intro
+  --   · apply sq_pos_iff.mpr
+  --     simp
+  --   · apply sq_le_sq.mpr
+  --     clear L h2 h1 α
+  --     rw [<- Int.dist_eq]
+  --     rw [Int.dist_eq']
+  --     have X :  ((query l₁ - query l₂).natAbs : ℝ) ≤ (Δ.val : ℝ) := by
+  --       apply Nat.cast_le.mpr
+  --       apply bounded_sensitivity
+  --     rw [abs_eq_natAbs]
+  --     simp only [abs_cast]
+  --     exact X
+  -- · apply mul_nonneg
+  --   · apply sq_nonneg
+  --   · apply sq_nonneg
+  -- · apply mul_nonneg
+  --   · apply mul_nonneg
+  --     · linarith
+  --     · apply sq_nonneg
+  --   · apply inv_nonneg.mpr
+  --     apply sq_nonneg
+  -- -/
 
 lemma discrete_gaussian_shift {σ : ℝ} (h : σ ≠ 0) (μ : ℝ) (τ x : ℤ) :
   discrete_gaussian σ μ (x - τ) = discrete_gaussian σ (μ + τ) (x) := by
