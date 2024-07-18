e8327add9f1a44570be47e3a9226a5d371b61be5
jtristan <trjohnb@amazon.com>
2024-07-18 13:51:13 -0400

    option for multiple mix

--- Tests/benchmarks.py
+++ Tests/benchmarks.py
@@ -30,22 +30,25 @@ Benchmark: Discrete Gaussians \n\
     # Values of epsilon attempted
     sigmas = []
 
-    # SampCert DiscreteGaussianSample
-    dg_mean = []
-    dg_stdev = []
+    g = GaussianDiscrete(epsilon=0.01, delta=0.00001)
 
-    # IBM sample_dgauss
+
+    # SampCert
+    means = []
+    stdevs = []
+    for i in mix:
+        means.append([])
+        stdevs.append([])
+
+    # sample_dgauss
     ibm_dg_mean = []
     ibm_dg_stdev = []
 
-    # IBM DiffPrivLib GaussianDiscrete
+    # DiffPrivLib GaussianDiscrete
     ibm_dpl_mean = []
     ibm_dpl_stdev = []
 
     num_attempts = warmup_attempts + measured_attempts
-
-    # the parameters do not matter, we will set the scale directly
-    g = GaussianDiscrete(epsilon=0.01, delta=0.00001)
 
     for sigma in tqdm.tqdm(np.linspace(lb+0.001,ub,quantity)):
         
@@ -55,15 +58,19 @@ Benchmark: Discrete Gaussians \n\
         sigma_num, sigma_denom = Decimal(sigma).as_integer_ratio()
         sigma_squared = sigma ** 2
 
-        t_dg = []
+        times = []
+        for i in mix:
+            times.append([])        
+
         t_ibm_dg = []
         t_ibm_dpl = []
-
-        for i in range(num_attempts):
-            start_time = timeit.default_timer()
-            sampler.DiscreteGaussianSample(sigma_num, sigma_denom, mix)
-            elapsed = timeit.default_timer() - start_time
-            t_dg.append(elapsed)
+         
+        for m in range(len(mix)): 
+            for i in range(num_attempts):
+                start_time = timeit.default_timer()
+                sampler.DiscreteGaussianSample(sigma_num, sigma_denom, mix[m])
+                elapsed = timeit.default_timer() - start_time
+                times[m].append(elapsed)
 
         for i in range(num_attempts):
             start_time = timeit.default_timer()
@@ -77,14 +84,17 @@ Benchmark: Discrete Gaussians \n\
             elapsed = timeit.default_timer() - start_time
             t_ibm_dpl.append(elapsed)
 
+        measured = []
         # Compute mean and stdev
-        dg_measured = np.array(t_dg[-measured_attempts:])
+        for m in range(len(mix)): 
+            measured.append(np.array(times[m][-measured_attempts:]))
         ibm_dg_measured = np.array(t_ibm_dg[-measured_attempts:])
         ibm_dpl_measured = np.array(t_ibm_dpl[-measured_attempts:])
 
         # Convert s to ms
-        dg_mean.append(dg_measured.mean() * 1000.0)
-        dg_stdev.append(dg_measured.std() * 1000.0)
+        for m in range(len(mix)): 
+            means[m].append(measured[m].mean() * 1000.0)
+            stdevs[m].append(measured[m].std() * 1000.0)
         ibm_dg_mean.append(ibm_dg_measured.mean() * 1000.0)
         ibm_dg_stdev.append(ibm_dg_measured.std() * 1000.0)
         ibm_dpl_mean.append(ibm_dpl_measured.mean() * 1000.0)
@@ -93,8 +103,8 @@ Benchmark: Discrete Gaussians \n\
 
     fig,ax1 = plt.subplots()
 
-    ax1.plot(sigmas, dg_mean, color='red', linewidth=1.0, label='DiscreteGaussianSample' + ' mix = ' + str(mix))
-    ax1.fill_between(sigmas, np.array(dg_mean)-0.5*np.array(dg_stdev), np.array(dg_mean)+0.5*np.array(dg_stdev),
+    ax1.plot(sigmas, means[0], color='red', linewidth=1.0, label='DiscreteGaussianSample' + ' mix = ' + str(mix))
+    ax1.fill_between(sigmas, np.array(means[0])-0.5*np.array(stdevs[0]), np.array(means[0])+0.5*np.array(stdevs[0]),
                      alpha=0.2, facecolor='k', linewidth=2, linestyle='dashdot', antialiased=True)
 
     ax1.plot(sigmas, ibm_dg_mean, color='blue', linewidth=1.0, label='IBM sample_dgauss')
@@ -115,7 +125,7 @@ Benchmark: Discrete Gaussians \n\
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--mix", type=int, help="mix", default=0)
+    parser.add_argument("--mix", nargs="+", type=int, help="mix", default=[0])
     parser.add_argument("--warmup", type=int, help="warmup", default=0)
     parser.add_argument("--trials", type=int, help="trials", default=1000)
     parser.add_argument("--min", type=int, help="min", default=1)
