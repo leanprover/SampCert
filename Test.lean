8693b8c4e4a6fdcee9e051cf6b95f1666f7c8ec6
jtristan <trjohnb@amazon.com>
2024-07-19 08:50:20 -0400

    histogram

--- Test.lean
+++ Test.lean
@@ -13,29 +13,72 @@ import SampCert.Samplers.Geometric.Code
 import SampCert.Samplers.Gaussian.Code
 import SampCert.Samplers.GaussianGen.Code
 
-open SLang Std
+open SLang Std Int Array
 
-def sample (dist : SLang ℤ) (numSamples : ℕ) : IO (List ℤ) := do
-  let mut samples : List ℤ := []
-  for _ in [:numSamples] do
+structure IntHistogram where
+  repr : Array ℕ
+  min : ℤ
+
+/--
+  sample `numSamples` times from `dist` into an array and keep track
+  of the minimum and maximum sample value
+-/
+def sample (dist : SLang ℤ) (numSamples : ℕ) : IO ((Array ℤ) × ℤ × ℤ) := do
+  if numSamples < 2 then
+    panic "sample: 2 samples at least required"
+  let mut samples : Array ℤ := mkArray numSamples 0
+  let s₁ : ℤ ← run <| dist
+  samples := samples.set! 0 s₁
+  let s₂ : ℤ ← run <| dist
+  samples := samples.set! 1 s₂
+  let mut min : ℤ := s₁
+  let mut max : ℤ := s₂
+  if s₂ < s₁ then
+    min := s₂
+    max := s₁
+  for i in [2:numSamples] do
     let s : ℤ ← run <| dist
-    samples := s :: samples
-  return samples
+    samples := samples.set! i s
+    if s < min then
+      min := s
+    else if s > max then
+      max := s
+  return (samples,min,max)
 
-def estimateMean (samples : List ℤ) : IO Float := do
+def estimateMean (samples : Array ℤ) : Float := Id.run do
   let mut acc : Float := 0
   for s in samples do
     acc := acc + Float.ofInt s
-  return acc / (samples.length).toFloat
+  return acc / (samples.size).toFloat
 
-def estimateVariance (samples : List ℤ) (mean : Float) : IO Float := do
+def estimateVariance (samples : Array ℤ) (mean : Float) : Float := Id.run do
   let mut acc : Float := 0
   for s in samples do
     acc := acc + (Float.ofInt s - mean)^2
-  return acc / ((samples.length).toFloat - 1)
+  return acc / ((samples.size).toFloat - 1)
+
+instance : DecidableRel Int.le := by
+  simp [DecidableRel]
+  intros a b
+  apply Int.decLe
+
+/--
+  compute histogram of `samples`
+-/
+def histogram (samples : Array ℤ) (min max : ℤ) : IntHistogram :=  Id.run do
+  if max < min then
+    panic "histogram: max less than min"
+  let mut hist : Array ℕ := mkArray (1 + max - min).toNat 0
+  for v in samples do
+    let idx := v - min
+    if idx < 0 then
+      panic "histogram: index less than 0"
+    hist := hist.set! idx.toNat (hist.get! idx.toNat + 1)
+  return { repr := hist, min := min }
 
 def main : IO Unit := do
-  let samples ← sample (DiscreteGaussianSample 1 1 7) 1000000
-  let mean ← estimateMean samples
-  let variance ← estimateVariance samples mean
-  IO.println s!"{mean} {variance}"
+  let (samples, min, max) ← sample (DiscreteGaussianSample 1 1 7) 10000
+  let mean := estimateMean samples
+  let variance := estimateVariance samples mean
+  let hist := histogram samples min max
+  IO.println s!"{mean} {variance} {hist.repr.size}"
