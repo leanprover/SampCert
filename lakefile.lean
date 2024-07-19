8bbd61879f648c7ae58879f6d77fe4cedb6ae9ae
jtristan <trjohnb@amazon.com>
2024-07-19 18:31:06 -0400

    -lstdc++

--- lakefile.lean
+++ lakefile.lean
@@ -22,7 +22,7 @@ target ffi.o pkg : FilePath := do
   let oFile := pkg.buildDir / "ffi.o"
   let srcJob ← inputTextFile <| pkg.dir / "ffi.cpp"
   let weakArgs := #["-I", (← getLeanIncludeDir).toString]
-  buildO oFile srcJob weakArgs #["-fPIC"] "g++" getLeanTrace
+  buildO oFile srcJob weakArgs #["-fPIC", "-lstdc++"] "g++" getLeanTrace
 
 extern_lib libleanffi pkg := do
   let ffiO ← ffi.o.fetch
