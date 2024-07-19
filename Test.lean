b2df7afd4be7589f4b7fb2bf71afba94f948c474
jtristan <trjohnb@amazon.com>
2024-07-19 16:56:19 -0400

    Tests

--- Test.lean
+++ Test.lean
@@ -23,7 +23,7 @@ structure IntHistogram where
 
 def IntHistogram.index (hist : IntHistogram) (i : ℤ) : ℤ := Id.run do
   if i - hist.min < 0 then
-    panic "IntHistogram.get!: index lower than min"
+    panic! "IntHistogram.get!: index lower than min"
   i + hist.min
 
 def histToSTring (hist : IntHistogram) : String  := Id.run do
@@ -43,7 +43,7 @@ instance : ToString IntHistogram where
 -/
 def sample (dist : SLang ℤ) (numSamples : ℕ) : IO ((Array ℤ) × ℤ × ℤ) := do
   if numSamples < 2 then
-    panic "sample: 2 samples at least required"
+    panic! "sample: 2 samples at least required"
   let mut samples : Array ℤ := mkArray numSamples 0
   let s₁ : ℤ ← run <| dist
   samples := samples.set! 0 s₁
@@ -68,12 +68,12 @@ def sample (dist : SLang ℤ) (numSamples : ℕ) : IO ((Array ℤ) × ℤ × ℤ
 -/
 def histogram (samples : Array ℤ) (min max : ℤ) : IO IntHistogram := do
   if max < min then
-    panic "histogram: max less than min"
+    panic! "histogram: max less than min"
   let mut hist : Array ℕ := mkArray (1 + max - min).toNat 0
   for v in samples do
     let idx := v - min
     if idx < 0 then
-      panic "histogram: index less than 0"
+      panic! "histogram: index less than 0"
     hist := hist.set! idx.toNat (hist.get! idx.toNat + 1)
   return { repr := hist, min := min, size := samples.size }
 
@@ -88,7 +88,7 @@ def estimateMean (hist : IntHistogram) : IO Float := do
 -/
 def estimateMoment (hist : IntHistogram) (mean : Float) (moment : ℕ) : IO Float := do
   if moment < 2 then
-    panic "estimateMoment: moment must be at least 2"
+    panic! "estimateMoment: moment must be at least 2"
   let mut acc : Float := 0
   for i in [:hist.repr.size] do
     for _ in [:hist.repr.get! i] do
@@ -112,7 +112,7 @@ def estimateKurtosis (hist : IntHistogram) (mean : Float) (variance : Float) : I
 -/
 def estimateCDF (hist : IntHistogram) : IO IntHistogram := do
   if hist.size = 0 then
-    panic "estimateCDF: empty histogram"
+    panic! "estimateCDF: empty histogram"
   let mut cdf : Array ℕ := mkArray hist.repr.size 0
   cdf := cdf.set! 0 <| hist.repr.get! 0
   for i in [1:cdf.size] do
@@ -147,10 +147,8 @@ def KolmogorovDistance (hist : IntHistogram) (num den : ℕ+) : IO Float := do
       max := d
   return max
 
-def main : IO Unit := do
-  let num : ℕ+ := 1
-  let den : ℕ+ := 1
-  let (samples, min, max) ← sample (DiscreteGaussianSample num den 7) 10000
+def test (num den : ℕ+) (mix numSamples : ℕ) (threshold : Float) : IO Unit := do
+  let (samples, min, max) ← sample (DiscreteGaussianSample num den mix) numSamples
   let hist ← histogram samples min max
   let mean ← estimateMean hist
   let variance ← estimateVariance hist mean
@@ -158,4 +156,41 @@ def main : IO Unit := do
   let kurtosis ← estimateSkewness hist mean variance
   let cdf ← estimateCDF hist
   let D ← KolmogorovDistance cdf num den
-  IO.println s!"{mean} {variance} {skewness} {kurtosis} {D}\n {cdf}"
+
+  if mean.abs > threshold then
+    panic! s!"mean = {mean}"
+  IO.println s!"mean = {mean}"
+
+  let trueVariance := (num : ℕ).toFloat^2 / (den : ℕ).toFloat^2
+  if (variance - trueVariance).abs > threshold then
+    panic! s!"variance = {variance}, true variance is {trueVariance}"
+  IO.println s!"variance = {variance}, true variance is {trueVariance}"
+
+  if skewness.abs > threshold then
+    panic! s!"skewness = {skewness}"
+  IO.println s!"skewness = {skewness}"
+
+  if kurtosis.abs > threshold then
+    panic! s!"kurtosis = {kurtosis}"
+  IO.println s!"kurtosis = {kurtosis}"
+
+  if D.abs > threshold then
+    panic! s!"Kolmogorov distance = {D}"
+  IO.println s!"Kolmogorov distance = {D}"
+
+
+def main : IO Unit := do
+  let tests : List (ℕ+ × ℕ+ × ℕ) := [
+    (1,1,0),
+    (1,1,7),
+    (1,1,10000000),
+    (1,2,0),
+    (1,2,7),
+    (1,2,10000000),
+    (2,1,0),
+    (2,1,7),
+    (2,1,10000000),
+  ]
+  for (num,den,mix) in tests do
+    IO.println s!"num = {(num : ℕ)}, den = {(den : ℕ)}, mix = {mix}"
+    test num den mix 1000000 0.1
