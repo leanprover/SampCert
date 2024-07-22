2660a82a22b12b6dd656d9bcf463cf70afa88fe0
jtristan <trjohnb@amazon.com>
2024-07-22 13:49:16 -0400

    Trying out FFI simplifications

--- ffi.cpp
+++ ffi.cpp
@@ -4,9 +4,9 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jean-Baptiste Tristan
 */
 #include <lean/lean.h>
-#include <iostream> 
-#include <bit>
-#include <chrono>
+//#include <iostream> 
+//#include <bit>
+//#include <chrono>
 #include <random>
 using namespace std; 
 
