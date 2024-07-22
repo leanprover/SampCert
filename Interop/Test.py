eec195e403104f3fed8520fffd680711198b49a0
jtristan <trjohnb@amazon.com>
2024-07-22 15:10:33 -0400

    Cleaned up paths in Python FFI

--- Interop/Test.py
+++ Interop/Test.py
@@ -1,31 +1,34 @@
 from ctypes import *
+from os.path import expanduser
+
+home = expanduser("~")
 
 # Loading Lean's libraries
-CDLL("/Users/trjohnb/.elan/toolchains/leanprover--lean4---v4.10.0-rc2/lib/lean/libInit_shared.dylib", RTLD_GLOBAL)
-lean = CDLL("/Users/trjohnb/.elan/toolchains/leanprover--lean4---v4.10.0-rc2/lib/lean/libleanshared.dylib", RTLD_GLOBAL)
+CDLL(home + "/.elan/toolchains/leanprover--lean4---v4.10.0-rc2/lib/lean/libInit_shared.dylib", RTLD_GLOBAL)
+lean = CDLL(home +  "/.elan/toolchains/leanprover--lean4---v4.10.0-rc2/lib/lean/libleanshared.dylib", RTLD_GLOBAL)
 
 # import-graph uses some Lake definitions that are available in the lake library, but 
 # Lean does not produce a dynamic version
 # One needs to create the library
 # On Mac, it looks like: #clang++ -fpic -shared -Wl,-force_load libLake.a -o libLake_shared.dylib -L . -lleanshared
 # If you are in the lib/lean directory of your toolchain
-CDLL("/Users/trjohnb/.elan/toolchains/leanprover--lean4---v4.10.0-rc2/lib/lean/libLake_shared.dylib", RTLD_GLOBAL)
+CDLL(home +  "/.elan/toolchains/leanprover--lean4---v4.10.0-rc2/lib/lean/libLake_shared.dylib", RTLD_GLOBAL)
 
-# Loading Mathlic and its dependencies
+# Loading Mathlib and its dependencies
 # To create the dynamic library, for each one of them, run lake build xxx:shared
-CDLL("/Users/trjohnb/Code/Lean/SampCert/.lake/build/lib/libleanffi.dylib", RTLD_GLOBAL) 
-CDLL("/Users/trjohnb/Code/Lean/SampCert/.lake/packages/cli/.lake/build/lib/libCli.dylib", RTLD_GLOBAL)
-CDLL("/Users/trjohnb/Code/Lean/SampCert/.lake/packages/batteries/.lake/build/lib/libBatteries.dylib", RTLD_GLOBAL)
-CDLL("/Users/trjohnb/Code/Lean/SampCert/.lake/packages/aesop/.lake/build/lib/libAesop.dylib", RTLD_GLOBAL)
-CDLL("/Users/trjohnb/Code/Lean/SampCert/.lake/packages/proofwidgets/.lake/build/lib/libProofWidgets.dylib", RTLD_GLOBAL)
-CDLL("/Users/trjohnb/Code/Lean/SampCert/.lake/packages/qq/.lake/build/lib/libQq.dylib", RTLD_GLOBAL)
-CDLL("/Users/trjohnb/Code/Lean/SampCert/.lake/packages/importGraph/.lake/build/lib/libImportGraph.dylib", RTLD_GLOBAL)
+CDLL("../.lake/build/lib/libleanffi.dylib", RTLD_GLOBAL) 
+CDLL("../.lake/packages/cli/.lake/build/lib/libCli.dylib", RTLD_GLOBAL)
+CDLL("../.lake/packages/batteries/.lake/build/lib/libBatteries.dylib", RTLD_GLOBAL)
+CDLL("../.lake/packages/aesop/.lake/build/lib/libAesop.dylib", RTLD_GLOBAL)
+CDLL("../.lake/packages/proofwidgets/.lake/build/lib/libProofWidgets.dylib", RTLD_GLOBAL)
+CDLL("../.lake/packages/qq/.lake/build/lib/libQq.dylib", RTLD_GLOBAL)
+CDLL("../.lake/packages/importGraph/.lake/build/lib/libImportGraph.dylib", RTLD_GLOBAL)
 
 # Loading SampCert's FFI
-CDLL("/Users/trjohnb/Code/Lean/SampCert/.lake/packages/mathlib/.lake/build/lib/libMathlib.dylib", RTLD_GLOBAL)
+CDLL("../.lake/packages/mathlib/.lake/build/lib/libMathlib.dylib", RTLD_GLOBAL)
 
 # Loading SampCert
-samplers = CDLL("/Users/trjohnb/Code/Lean/SampCert/.lake/build/lib/libSamplers.dylib", RTLD_GLOBAL) 
+samplers = CDLL("../.lake/build/lib/libSamplers.dylib", RTLD_GLOBAL) 
 
 # Initialization
 lean.lean_initialize_runtime_module()
