4be0701110b6b1b61d742b6e3b4a7c7288f2b867
rshlyakh <robert.shlyakhtenko@icloud.com>
2025-07-01 14:06:59 -0700

    forethan

--- SampCert/DifferentialPrivacy/Local/RandomizedResponse.lean
+++ SampCert/DifferentialPrivacy/Local/RandomizedResponse.lean
@@ -6,14 +6,32 @@ import SampCert
 
 /- Implementation of randomized response for a single user with rational parameter 0 ≤ lambda ≤ 1/2 -/
 
-def RRRandomizer (query : T -> Bool) (l : T) (num : Nat) (den : PNat) (h: 2 * num < den) : SLang Bool := do
+/- def RRRandomizer (query : T -> Bool) (l : T) (num : Nat) (den : PNat) (h: 2 * num < den) : SLang Bool := do
   let result1 ← SLang.BernoulliSample (2 * num + den) (2 * den) (by
     linarith) -- note (2 * num + den) / (2 * den) = 1/2 + num/den
   match result1 with
    | true => return (query l) -- happens with probability (1/2 + num/den)
    | false => return ¬(query l) -- happens with probability (1/2 - num/den)
 
-lemma RRRandomizerpmf_lemma (query : T -> Bool) (l : T) (num : Nat) (den : PNat) (h : 2 * num < den):
+-/
+
+/- What return actually does is that it returns that Dirac distribution that is either all true or all false...
+   So this is wrong?
+-/
+
+/- Note that this doesn't work: -/
+
+/- def RRRandomizertest (query : T -> Bool) (l : T) (num : Nat) (den : PNat) (h: 2 * num < den) : Bool -> ENNReal :=
+  fun b =>
+    let result1 := SLang.BernoulliSample (2 * num + den) (2 * den) (by
+    linarith)
+    match b with
+    | true => query l
+    | false => ¬ query l
+
+-/
+
+/- lemma RRRandomizerpmf_lemma (query : T -> Bool) (l : T) (num : Nat) (den : PNat) (h : 2 * num < den):
   HasSum (RRRandomizer query l num den h) 1 := by
   simp [RRRandomizer]
   simp [(Summable.hasSum_iff ENNReal.summable), tsum_bool, add_tsub_cancel_iff_le]
@@ -23,6 +41,8 @@ lemma RRRandomizerpmf_lemma (query : T -> Bool) (l : T) (num : Nat) (den : PNat)
 
 def RRRandomizerPMF (query : T -> Bool) (l : T) (num : Nat) (den : PNat) (h: 2 * num < den) : PMF Bool :=
   ⟨RRRandomizer query l num den h, RRRandomizerpmf_lemma query l num den h⟩
+
+-/
 
 /-- THERE ARE STILL ISSUES BELOW THIS LINE. DO NOT EDIT YET. -/
 
@@ -60,27 +80,34 @@ noncomputable def output_probabilities {T : Type} [DecidableEq T] (num : ℕ) (d
   fun l' =>
     let diff := count_differences (List.map (fun x => x) l) l'
     let same := l.length - diff
-    ENNReal.ofReal (((2 * num + den : ℝ) / (2 * den : ℝ)) ^ diff * ((den - 2 * num : ℝ) / (2 * den : ℝ)) ^ same)
-    /-- The output_probabilities function describes a function from List Bool to ENNReal,
-      but SLang (List Bool) is a function List Bool → ENNReal (i.e., a probability mass function).
-      To return SLang (List T), you should define a function of type List T → ENNReal.
-      If you want to return SLang (List T), you can define: -/
+    (((2 * num + den) / (2 * den)) ^ diff * ((den - 2 * num) / (2 * den)) ^ same : ENNReal)
+
 noncomputable def output_probabilities_SLang {T : Type} [DecidableEq T] (num : ℕ) (den : PNat) (_ : 2 * num < den) (l : List T) : SLang (List T) :=
   fun l' =>
     let diff := count_differences l l'
     let same := l.length - diff
     ENNReal.ofReal (((2 * num + den : ℝ) / (2 * den : ℝ)) ^ diff * ((den - 2 * num : ℝ) / (2 * den : ℝ)) ^ same)
 
-noncomputable def RandomizedResponseSample {T : Type} [DecidableEq T] (query: T -> Bool) (l : List T) (num : Nat) (den : PNat) (h: 2 * num < den) : (SLang (List Bool)) :=
-  output_probabilities num den h (List.map query l)
+noncomputable def RandomizedResponseSample {T : Type} [DecidableEq T] (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : List T) : PMF (List Bool) :=
+  ⟨output_probabilities num den h (List.map query l), by sorry⟩
 
-def CoinFlipSample (query : T -> Bool) (l : T): SLang (Bool):= do
+lemma log_arith: Real.log 3 ≥ 0 := Real.log_nonneg (by linarith)
+
+lemma RRPureDP {T : Type} [DecidableEq T] (query : T -> Bool) (l : List T) (num : Nat) (den : PNat) (h: 2 * num < den): SLang.PureDP (RandomizedResponseSample query num den h) ((Real.log 3).toNNReal) :=
+  by
+    rw[SLang.PureDP]
+    rw[SLang.DP]
+    intro l₁ l₂ hl₁l₂ S
+    /- Study SampCert PureDP proofs -/
+    sorry
+
+
+
+/- def CoinFlipSample (query : T -> Bool) (l : T): SLang (Bool):= do
   RRRandomizer query l 1 4 (by decide) /- Randomized Response with parameter 1/4 -/
 
-lemma coinflippmf_lemma (query: T -> Bool) (l: T) : HasSum (CoinFlipSample query l) 1 :=
-  RRRandomizerpmf_lemma query l 1 4 (by decide)
-
 def CoinFlipPMF (query : T -> Bool) (l : T) : PMF Bool :=
-  ⟨CoinFlipSample query l, coinflippmf_lemma query l⟩
+  ⟨CoinFlipSample query l, RRRandomizerpmf_lemma query l 1 4 (by decide)⟩
 
 /- need randomized response as a mechanism... -/
+-/
