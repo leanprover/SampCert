4ee3680c5d89cc3d3f5726910f21e36335f34b08
rshlyakh <robert.shlyakhtenko@icloud.com>
2025-07-11 16:24:22 -0700

    beforepull

--- SampCert/DifferentialPrivacy/Pure/Local/RandomizedResponse/Definitions.lean
+++ SampCert/DifferentialPrivacy/Pure/Local/RandomizedResponse/Definitions.lean
@@ -15,6 +15,11 @@ def RRSingleSample  {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2
   let r ← SLang.BernoulliSample (den - 2*num) (2 * den) (arith_0 num den h)
   return Bool.xor (query l) r
 
+def Y (query : T -> Bool): Bool -> (T -> Bool) := fun r => (fun l => Bool.xor (query l) r)
+/- Y is a random variable which outputs the function measuring whether or not a given person
+   changes their answer. It is distributed according to the probability distribution
+   from which we sample r.-/
+
 def RRSample {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num ≤ den) (l : List T) : SLang (List Bool) := do
 /- RRSample uses monadic map to apply RRSingleSample2 on an entire dataset. -/
  l.mapM (fun x => RRSingleSample query num den h x)
