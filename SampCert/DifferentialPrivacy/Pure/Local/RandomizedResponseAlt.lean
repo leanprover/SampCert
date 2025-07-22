99bfa27880865875d790dadd572cefebce86b8b5
rshlyakh <robert.shlyakhtenko@icloud.com>
2025-07-21 19:16:52 -0700

    moreorganization

--- SampCert/DifferentialPrivacy/Pure/Local/RandomizedResponseAlt.lean
+++ SampCert/DifferentialPrivacy/Pure/Local/RandomizedResponseAlt.lean
@@ -4,6 +4,7 @@ import SampCert.DifferentialPrivacy.Pure.DP
 import SampCert.Samplers.Bernoulli.Properties
 import SampCert.DifferentialPrivacy.Pure.Local.LawfulMonadSLang
 import SampCert.DifferentialPrivacy.Pure.Local.LocalDP.DPwithUpdateNeighbour
+import SampCert.DifferentialPrivacy.Pure.Local.RandomizedResponse.BasicLemmas
 import SampCert.DifferentialPrivacy.Pure.Local.RandomizedResponse.DPProof
 import SampCert.DifferentialPrivacy.Pure.Local.RandomizedResponse.PMFProperties
 import SampCert.DifferentialPrivacy.Pure.Local.ENNRealLemmasSuite
@@ -12,128 +13,6 @@ import SampCert.DifferentialPrivacy.Pure.Local.ENNRealLemmasSuite
 open SLang
 open ENNRealLemmas
 open RandomizedResponse
-/- open MultiBernoulli -/
-
-lemma pnat_zero_imp_false (den : PNat): (den : Nat) = 0 -> False := by aesop
-
-lemma RRSingleSample_true_true {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : T) (hq : query l = true):
-  RRSingleSample query num den h l true = (den + 2 * num) / (2 * den) := by
-  rw[RRSingleSample]
-  aesop
-  sorry /- This is arithmetically true, but proving arithmetic things is a mess -/
-
-lemma RRSingleSample_true_false {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : T) (hq : query l = true):
-  RRSingleSample query num den h l false = (den - 2 * num) / (2 * den) := by
-  rw[RRSingleSample]
-  simp_all only [bind, pure, Bool.true_bne, bind_apply, BernoulliSample_apply, ENNReal.natCast_sub, Nat.cast_mul,
-    Nat.cast_ofNat, PNat.mul_coe, PNat.val_ofNat, pure_apply, Bool.false_eq, Bool.not_eq_false', mul_ite, ↓reduceIte,
-    mul_one, mul_zero, tsum_ite_eq, NNReal.ofPNat, Nonneg.mk_natCast]
-  apply Eq.refl
-
-lemma RRSingleSample_false_true {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : T) (hq : query l = false):
-  RRSingleSample query num den h l true = (den - 2 * num) / (2 * den) := by
-  rw[RRSingleSample]
-  simp_all only [bind, pure, Bool.false_bne, bind_apply, BernoulliSample_apply, ENNReal.natCast_sub, Nat.cast_mul,
-    Nat.cast_ofNat, PNat.mul_coe, PNat.val_ofNat, pure_apply, Bool.true_eq, Bool.not_eq_true', mul_ite,
-    Bool.false_eq_true, ↓reduceIte, mul_one, mul_zero, tsum_ite_eq, NNReal.ofPNat, Nonneg.mk_natCast]
-  apply Eq.refl
-
-lemma RRSingleSample_false_false {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : T) (hq : query l = false):
-  RRSingleSample query num den h l false = (den + 2 * num) / (2 * den) := by
-  rw[RRSingleSample]
-  simp_all only [bind, pure, Bool.false_bne, bind_apply, BernoulliSample_apply, ENNReal.natCast_sub, Nat.cast_mul,
-    Nat.cast_ofNat, PNat.mul_coe, PNat.val_ofNat, pure_apply, Bool.false_eq, mul_ite, Bool.false_eq_true, ↓reduceIte,
-    mul_one, mul_zero, tsum_ite_eq, NNReal.ofPNat, Nonneg.mk_natCast]
-  /- This is the same state as the first lemma that's not working,
-     again it's just annoying arithmetic. -/
-  sorry
-
-lemma RRSingleSample_non_zero {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : T) (b : Bool):
-  RRSingleSample query num den h l b ≠ 0 := by
-  simp [RRSingleSample]
-  cases hb : b == query l with
-  | true => simp at hb
-            subst hb
-            simp
-            rw [@tsub_eq_zero_iff_le]
-            rw [@Mathlib.Tactic.PushNeg.not_le_eq]
-            apply quot_lt_one_rev
-            norm_cast
-            rw [PNat.mul_coe]
-            simp_all only [PNat.val_ofNat]
-            sorry
-  | false => simp at hb
-             rw [← Bool.eq_not_of_ne hb]
-             intro j
-             apply And.intro
-             trivial
-             apply And.intro
-             {norm_cast
-              rw [@Nat.sub_eq_zero_iff_le]
-              linarith
-            }
-             {exact ne_of_beq_false rfl}
-
-lemma RRSingleSample_finite {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : T) (b : Bool):
-  RRSingleSample query num den h l b ≠ ⊤ := by
-  have hden: ↑(NNReal.ofPNat den) ≠ (0 : ENNReal) := by
-                rw [@ne_iff_lt_or_gt]
-                apply Or.inr
-                simp_all only [NNReal.ofPNat, Nonneg.mk_natCast, gt_iff_lt, ENNReal.coe_pos, Nat.cast_pos]
-                apply den.2
-  cases hb: b with
-  | true => cases hq: query l with
-      | true => rw [RRSingleSample_true_true _ _ _ _ _ hq]
-                apply div_ne_top
-                exact Ne.symm (ne_of_beq_false rfl)
-                refine mult_ne_zero 2 ↑(NNReal.ofPNat den) ?true.true.h2.h1 ?true.true.h2.h2
-                aesop
-                exact hden
-      | false => rw [RRSingleSample_false_true _ _ _ _ _ hq]
-                 apply div_ne_top
-                 aesop
-                 refine mult_ne_zero 2 ↑(NNReal.ofPNat den) ?true.false.h2.h1 ?true.false.h2.h2
-                 aesop
-                 exact hden
-  | false => cases hq: query l with
-      | true => rw [RRSingleSample_true_false _ _ _ _ _ hq]
-                apply div_ne_top
-                aesop
-                refine mult_ne_zero 2 ↑(NNReal.ofPNat den) ?false.true.h2.h1 ?false.true.h2.h2
-                aesop
-                exact hden
-      | false => rw [RRSingleSample_false_false _ _ _ _ _ hq]
-                 apply div_ne_top
-                 rw [@ENNReal.add_ne_top]
-                 apply And.intro
-                 aesop
-                 exact Ne.symm (ne_of_beq_false rfl)
-                 refine mult_ne_zero 2 ↑(NNReal.ofPNat den) ?false.false.h2.h1 ?false.false.h2.h2
-                 aesop
-                 exact hden
-
-lemma RRSample_diff_lengths {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l₁ : List T) (l₂ : List Bool) (hlen : l₁.length ≠ l₂.length):
-  RRSample query num den h l₁ l₂= 0 := by
-  induction l₁ generalizing l₂ with
-  | nil => simp [RRSample, -mapM]
-           aesop
-  | cons hd tl ih =>
-  simp [RRSample, -mapM]
-  simp [RRSample, -mapM] at ih
-  apply And.intro
-  apply Or.inr
-  intro b
-  intro a
-  subst a
-  simp_all only [mapM, List.length_cons, ne_eq, add_left_inj, not_false_eq_true]
-  apply Or.inr
-  intro b
-  intro a
-  subst a
-  simp_all only [mapM, List.length_cons, ne_eq, add_left_inj, not_false_eq_true]
-
-lemma RRSamplePMF_diff_lengths {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h : 2 * num < den) (l₁ : List T) (l₂ : List Bool) (hlen : l₁.length ≠ l₂.length):
-  RRSample_PMF query num den h l₁ l₂ = 0 := RRSample_diff_lengths query num den h l₁ l₂ hlen
 
 namespace SLang
 lemma simplifier_1 (f : T -> SLang Bool):
