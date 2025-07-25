c78060c65fd3d21e821f769956eb36fde7a578fa
rshlyakh <robert.shlyakhtenko@icloud.com>
2025-07-25 14:20:52 -0700

    equivDPdefs

---
+++ SampCert/DifferentialPrivacy/Pure/Local/LocalDP/DPNeighbour.lean
@@ -0,0 +1,17 @@
+import SampCert
+import SampCert.DifferentialPrivacy.Pure.Local.LocalDP.DPwithGeneralNeighbour
+import SampCert.DifferentialPrivacy.Neighbours
+
+namespace SLang
+
+open SLang
+
+/- Abstraction of SampCert definition of DP-/
+def DP_withARUNeighbour (m : Mechanism T U) (ε : ℝ) : Prop :=
+  DP_withGeneralNeighbour m (Neighbour) ε
+
+/- Proof that our definitions are equivalent -/
+theorem DP_withARUNeighbour_isDP (m : Mechanism T U) (ε : ℝ) :
+  DP_withARUNeighbour m ε ↔ DP m ε := by simp [DP_withARUNeighbour, DP_withGeneralNeighbour, DP]
+
+end SLang
