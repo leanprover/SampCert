58e0a15a6183757b4a3512a61a320977321ffb62
Arasyilmaz1 <ayilmaz26@cornellcollege.edu>
2025-08-05 16:13:59 -0700

    Shuffle Model Implementation

--- SampCert/DifferentialPrivacy/Pure/Local/ShuffleModel/Definitions.lean
+++ SampCert/DifferentialPrivacy/Pure/Local/ShuffleModel/Definitions.lean
@@ -1,27 +1,24 @@
 import SampCert.DifferentialPrivacy.Pure.Local.RandomizedResponse.Definitions
 import SampCert.Samplers.Uniform.Code
+import SampCert.Samplers.Uniform.Properties
+import SampCert.DifferentialPrivacy.Pure.Local.RandomizedResponse.Definitions
 
 namespace SLang
 
 
-def swapAt {α : Type} (arr : Array α) (i j : Nat) : Array α :=
-  if h1 : i < arr.size ∧ j < arr.size then
-    let tmp := arr[i]
-    arr.set! i arr[j] |>.set! j tmp
-  else
-    arr
-
-
-def single_swap {T:Type}(l: Array T)(i: PNat) := do
-  let j ← UniformSample i
-  return l.swap ⟨i, sorry⟩ ⟨j, sorry⟩
-
-#check single_swap
-
+def UniformSample2 (n : PNat) : IO (Fin n) := do
+  let r ← IO.rand 0 (n.val - 1)
+  return ⟨r, by decide⟩
 
 def Shuffler {α: Type}(l:List α) := do
   let mut a := l.toArray
+  let mut b : Array α := Array.empty
   for h: i in [1:a.size] do
-   let j ← UniformSample (Nat.toPNat' i)
-   a := a.swap ⟨i, sorry⟩ ⟨j, sorry⟩
-  return a
+   let j ← UniformSample (Nat.toPNat' i+1)
+
+   b := a.swap ⟨i, by aesop; exact Membership.get_elem_helper h rfl⟩ ⟨j, by sorry⟩
+  return b.toList
+
+def ShuffleModel(query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den)(l: List T) := do
+  let l ← RandomizedResponse.RRSample query num den h l
+  return Shuffler l
