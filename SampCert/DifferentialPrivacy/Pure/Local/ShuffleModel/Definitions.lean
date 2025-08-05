1def98682eb09d8401ed674d0414d2c4e02ae6fb
Arasyilmaz1 <ayilmaz26@cornellcollege.edu>
2025-08-05 09:17:11 -0700

    Shuffler update

--- SampCert/DifferentialPrivacy/Pure/Local/ShuffleModel/Definitions.lean
+++ SampCert/DifferentialPrivacy/Pure/Local/ShuffleModel/Definitions.lean
@@ -4,20 +4,31 @@ import SampCert.Samplers.Uniform.Code
 namespace SLang
 
 
+def swapAt {α : Type} (arr : Array α) (i j : Nat) : Array α :=
+  if h1 : i < arr.size ∧ j < arr.size then
+    let tmp := arr[i]
+    arr.set! i arr[j] |>.set! j tmp
+  else
+    arr
 
 
-
-def single_swap {T:Type}(l:Array T)(i: PNat)(h: i < l.size) := do
+def single_swap {T:Type}(l: Array T)(i: PNat) := do
   let j ← UniformSample i
-  return l.swap ⟨i.val, h⟩ ⟨j, sorry⟩
+  return l.swap ⟨i, sorry⟩ ⟨j, sorry⟩
+
+#check single_swap
 
 
+def Shuffler {α: Type}(l:List α) := do
+  let mut a := l.toArray
+  for h: i in [1:a.size] do
+   let j ← UniformSample (Nat.toPNat' i)
+   a := a.swap ⟨i, sorry⟩ ⟨j, sorry⟩
+  return a
 
+def Shuffler{α: Type}(l:Array α) : SLang (List α) :=  do
+  let j ← UniformSample (Nat.toPNat' i)
 
-def Shuffler {α: Type}(l:List α): SLang (List α) :=
-  let a := l.toArray
-  sorry
-
-
-def reverseIterSafe {α : Type} (a : Array α) : Array α :=
-  Array.range a.size |>.reverse |>.map (λ i => a.get! i)
+  match l with
+  | [] => []
+  | hd::tl =>  hd.index
