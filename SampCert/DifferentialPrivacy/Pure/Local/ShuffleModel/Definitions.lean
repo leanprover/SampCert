ba7f815727569ce34ac28a4775e6af8a1bdf5caa
Arasyilmaz1 <ayilmaz26@cornellcollege.edu>
2025-08-06 16:26:23 -0700

    Shuffler Sorry

--- SampCert/DifferentialPrivacy/Pure/Local/ShuffleModel/Definitions.lean
+++ SampCert/DifferentialPrivacy/Pure/Local/ShuffleModel/Definitions.lean
@@ -11,13 +11,32 @@ namespace SLang
 open SLang
 open RandomizedResponse
 
+
+def UniformSample' (n : PNat) : SLang (Fin n) := do
+  let r ← UniformSample n
+  return (r : Fin n)
+
 def Shuffler {α: Type}(l:List α) := do
   let mut a := l.toArray
   let mut b : Array α := Array.empty
   for h: i in [1:a.size] do
-   let j ← UniformSample (Nat.toPNat' i+1)
+   let j ← UniformSample' (Nat.toPNat' i+1)
 
-   b := a.swap ⟨i, by aesop; exact Membership.get_elem_helper h rfl⟩ ⟨j, by sorry⟩
+   b := a.swap ⟨i, by aesop; exact Membership.get_elem_helper h rfl⟩ ⟨j, by
+   aesop
+   have h1: j ≤ i := by
+    rw [@Fin.le_iff_val_le_val]
+    norm_num
+    aesop
+    have h1: j.val < i.toPNat' + 1 := j.2
+    aesop
+    rw[← Nat.lt_add_one_iff]
+    exact h1
+    linarith[h.1]
+
+   have h2: i < l.length := by exact Membership.get_elem_helper h rfl
+   exact Nat.lt_of_le_of_lt h1 (by aesop)
+   ⟩
   return b.toList
 
 def ShuffleModel(query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den)(l: List T) := do
@@ -25,12 +44,11 @@ def ShuffleModel(query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den)(l
   let b ←  Shuffler l
   return b
 
-
 lemma ShuffleModel_PMF_helper {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : List T) :
   HasSum (ShuffleModel query num den h l) 1 := by sorry
 
 def ShuffleModel_PMF {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : List T) : PMF (List Bool) :=
   ⟨ShuffleModel query num den h l ,ShuffleModel_PMF_helper query num den h l⟩
 
-theorem ShuffleDP (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) :
+theorem Shuffle_is_DP (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) :
 DP_withUpdateNeighbour (ShuffleModel_PMF query num den h) (Real.log ((2⁻¹ + ↑num / ↑↑↑den) / (2⁻¹ - ↑num / ↑↑↑den))) := by sorry
