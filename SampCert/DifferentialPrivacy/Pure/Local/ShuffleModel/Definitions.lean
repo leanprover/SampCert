bc8c47363bdf501f1f628da301a861aaa7c9eef9
Renee Tetlow <tetlow01@allegheny.edu>
2025-08-14 16:39:42 -0400

    cleaned defs

--- SampCert/DifferentialPrivacy/Pure/Local/ShuffleModel/Definitions.lean
+++ SampCert/DifferentialPrivacy/Pure/Local/ShuffleModel/Definitions.lean
@@ -21,26 +21,27 @@ match l with
     return rest.insertNth i hd
 #check Shuffler
 
-
-
-
-
 def BinomialSample (seed: MultiBernoulli.SeedType)(n:PNat) := do
   let seeds := List.replicate n seed
   let list ← MultiBernoulli.MultiBernoulliSample (seeds)
   let k := List.count true list
   return k
 
-
-
-
-
-
-
-
-
-
-
+theorem BinomialSample_norms [LawfulMonad SLang] (seed : MultiBernoulli.SeedType) (n : PNat) :
+  HasSum (BinomialSample seed n) 1 := by
+  rw [BinomialSample]
+  simp
+  unfold probBind
+  simp [Summable.hasSum_iff ENNReal.summable]
+  have h: (push_forward (MultiBernoulli.MultiBernoulliSample (List.replicate (↑n) seed))
+        (fun (a : List Bool) => (List.count true a))) = (fun (b : Nat) =>
+        (∑' (a : List Bool), if b = List.count true a then MultiBernoulli.MultiBernoulliSample
+        (List.replicate (↑n) seed) a else 0)) := by
+          unfold push_forward
+          rfl
+  rw [← h]
+  rw [push_forward_prob_is_prob]
+  simp [MultiBernoulli.MultiBernoulliSample_normalizes]
 
 theorem BinomialSample_kprob (seed: MultiBernoulli.SeedType) (n : PNat) (k : Nat) :
   BinomialSample seed n k = ((n: Nat).choose k) * ((num / den) ^ k) * ((1 - (num / den)) ^ (n - k)) := by
@@ -50,146 +51,3 @@ theorem BinomialSample_kprob (seed: MultiBernoulli.SeedType) (n : PNat) (k : Nat
   simp [pure, bind]
 
   sorry
-
-
-
-
-
-/- This is the Shuffle Model. -/
-def ShuffleModel(query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den)(l: List T) := do
-  let l ← RandomizedResponse.RRSample query num den h l
-  let b ← Shuffler l
-  return b
-
-#check ShuffleModel
-
-
-lemma Shuffle_norms [LawfulMonad SLang] {α : Type}(l: List α): HasSum (Shuffler l) 1 := by
-  rw [Summable.hasSum_iff ENNReal.summable]
-  unfold Shuffler
-  rename_i inst
-  simp_all only [bind, PNat.add_coe, PNat.val_ofNat, pure, pure_bind, Array.toList_eq, bind_apply, pure_apply,
-    mul_ite, mul_one, mul_zero]
-  unfold probBind
-  simp [pure, bind]
-  sorry
-
-lemma one_step {α: Type}[BEq α](hd: α)(tl: List α)(l: List α)(h: List.isPerm (hd::tl) l): Shuffler (hd::tl) l = Shuffler tl (l.erase hd) / (tl.length+1) := by
-  unfold Shuffler
-  simp[probBind,pure,pure_apply]
-  have h: (List.toArray (hd::tl)).size = (List.toArray tl).size+1 := by
-    simp
-  rename_i inst h_1
-  rw[tsum_eq_single (List.toArray l)]
-  rw[tsum_eq_single (List.toArray l)]
-  simp
-  unfold UniformSample'
-  simp
-  aesop
-
-
-
-
-
-
-
-
-lemma Shuffle_permutes {α: Type} [BEq α] (l₁ l₂: List α)(hlen: n = l₁.length)(hlen2: n = l₂.length)(h: List.isPerm l₁ l₂): Shuffler l₁ l₂ = 1/Nat.factorial n := by
-  induction l₁ generalizing l₂ n with
-  | nil =>
-    simp [List.isPerm] at h
-    have h1: l₂ = [] := by sorry
-    subst h1
-    sorry
-  | cons hd tl ih =>
-    simp [List.isPerm] at h
-    have h1: Shuffler tl (l₂.erase hd) = 1 / (tl.length).factorial := by
-      rw [ih (l₂.erase hd)]
-      rfl
-      have h2: tl.length = n - 1 := by simp[hlen]
-      rw [h2]
-      have h3: (l₂.erase hd).length = l₂.length - 1 := by sorry
-      rw [h3]
-      sorry
-      exact h.right
-    rw[one_step]
-    rw[ih]
-
-
-
- /- induction n generalizing l₁ l₂
-  case zero =>
-    simp
-    symm at hlen
-    rw[List.length_eq_zero] at hlen
-    symm at hlen2
-    rw[List.length_eq_zero] at hlen2
-    rw[hlen, hlen2]
-    simp [Shuffler]
-    aesop
-    sorry
-  case succ x ih =>
-
-    cases h
-    case nil =>
-      simp at hlen
-    case cons hd tl₁ tl₂ h =>
-      simp at hlen
-      simp at hlen2
-      sorry
- -/
-
-
-/--/
-    cases l₁
-    case nil =>
-      simp at hlen
-    case cons hd tl =>
-      cases l₂
-      case nil =>
-        simp at hlen2
-      case cons hd2 tl2 =>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-lemma ShuffleModel_PMF_helper (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den)(l: List T) :
-HasSum (ShuffleModel query num den h l) 1 := by
-  rw [Summable.hasSum_iff ENNReal.summable]
-  unfold ShuffleModel
-  simp [pure,bind]
-  unfold RandomizedResponse.RRSample
-  sorry
-
-
-def ShuffleModel_PMF {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : List T) : PMF (List Bool) :=
-  ⟨ShuffleModel query num den h l ,ShuffleModel_PMF_helper query num den h l⟩
-
-
-def ShuffleModel_is_privPostProcess (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den)(l: List T) :
-ShuffleModel query num den h l = privPostProcess (RandomizedResponse.RRSample query num den h l) (Shuffler) (l) := by
-  unfold ShuffleModel
-  unfold RandomizedResponse.RRSample
-  simp [privPostProcess]
-  sorry
-
-theorem Shuffle_is_DP (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) :
-DP_withUpdateNeighbour (ShuffleModel_PMF query num den h) (Real.log ((2⁻¹ + ↑num / ↑↑↑den) / (2⁻¹ - ↑num / ↑↑↑den))) := by
-  unfold ShuffleModel_PMF
-  unfold ShuffleModel
-  simp [pure, bind]
-  unfold probBind
-
-  sorry
-
-end SLang
--- SampCert/DifferentialPrivacy/Pure/Local/ShuffleModel/junk.lean
+++ SampCert/DifferentialPrivacy/Pure/Local/ShuffleModel/junk.lean
@@ -91,3 +91,133 @@ def Shuffler2 {α: Type}(l:List α) := do
    exact Nat.lt_of_le_of_lt h1 (by aesop)
    ⟩
   return a.toList
+
+  /- This is the Shuffle Model. -/
+def ShuffleModel(query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den)(l: List T) := do
+  let l ← RandomizedResponse.RRSample query num den h l
+  let b ← Shuffler l
+  return b
+
+lemma Shuffle_norms [LawfulMonad SLang] {α : Type}(l: List α): HasSum (Shuffler l) 1 := by
+  rw [Summable.hasSum_iff ENNReal.summable]
+  unfold Shuffler
+  rename_i inst
+  simp_all only [bind, PNat.add_coe, PNat.val_ofNat, pure, pure_bind, Array.toList_eq, bind_apply, pure_apply,
+    mul_ite, mul_one, mul_zero]
+  unfold probBind
+  simp [pure, bind]
+  sorry
+
+lemma one_step {α: Type}[BEq α](hd: α)(tl: List α)(l: List α)(h: List.isPerm (hd::tl) l): Shuffler (hd::tl) l = Shuffler tl (l.erase hd) / (tl.length+1) := by
+  unfold Shuffler
+  simp[probBind,pure,pure_apply]
+  have h: (List.toArray (hd::tl)).size = (List.toArray tl).size+1 := by
+    simp
+  rename_i inst h_1
+  rw[tsum_eq_single (List.toArray l)]
+  rw[tsum_eq_single (List.toArray l)]
+  simp
+  unfold UniformSample'
+  simp
+  aesop
+
+
+lemma Shuffle_permutes {α: Type} [BEq α] (l₁ l₂: List α)(hlen: n = l₁.length)(hlen2: n = l₂.length)(h: List.isPerm l₁ l₂): Shuffler l₁ l₂ = 1/Nat.factorial n := by
+  induction l₁ generalizing l₂ n with
+  | nil =>
+    simp [List.isPerm] at h
+    have h1: l₂ = [] := by sorry
+    subst h1
+    sorry
+  | cons hd tl ih =>
+    simp [List.isPerm] at h
+    have h1: Shuffler tl (l₂.erase hd) = 1 / (tl.length).factorial := by
+      rw [ih (l₂.erase hd)]
+      rfl
+      have h2: tl.length = n - 1 := by simp[hlen]
+      rw [h2]
+      have h3: (l₂.erase hd).length = l₂.length - 1 := by sorry
+      rw [h3]
+      sorry
+      exact h.right
+    rw[one_step]
+    rw[ih]
+
+
+
+ /- induction n generalizing l₁ l₂
+  case zero =>
+    simp
+    symm at hlen
+    rw[List.length_eq_zero] at hlen
+    symm at hlen2
+    rw[List.length_eq_zero] at hlen2
+    rw[hlen, hlen2]
+    simp [Shuffler]
+    aesop
+    sorry
+  case succ x ih =>
+
+    cases h
+    case nil =>
+      simp at hlen
+    case cons hd tl₁ tl₂ h =>
+      simp at hlen
+      simp at hlen2
+      sorry
+ -/
+
+
+/--/
+    cases l₁
+    case nil =>
+      simp at hlen
+    case cons hd tl =>
+      cases l₂
+      case nil =>
+        simp at hlen2
+      case cons hd2 tl2 =>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+lemma ShuffleModel_PMF_helper (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den)(l: List T) :
+HasSum (ShuffleModel query num den h l) 1 := by
+  rw [Summable.hasSum_iff ENNReal.summable]
+  unfold ShuffleModel
+  simp [pure,bind]
+  unfold RandomizedResponse.RRSample
+  sorry
+
+
+def ShuffleModel_PMF {T : Type} (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) (l : List T) : PMF (List Bool) :=
+  ⟨ShuffleModel query num den h l ,ShuffleModel_PMF_helper query num den h l⟩
+
+
+def ShuffleModel_is_privPostProcess (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den)(l: List T) :
+ShuffleModel query num den h l = privPostProcess (RandomizedResponse.RRSample query num den h l) (Shuffler) (l) := by
+  unfold ShuffleModel
+  unfold RandomizedResponse.RRSample
+  simp [privPostProcess]
+  sorry
+
+theorem Shuffle_is_DP (query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den) :
+DP_withUpdateNeighbour (ShuffleModel_PMF query num den h) (Real.log ((2⁻¹ + ↑num / ↑↑↑den) / (2⁻¹ - ↑num / ↑↑↑den))) := by
+  unfold ShuffleModel_PMF
+  unfold ShuffleModel
+  simp [pure, bind]
+  unfold probBind
+
+  sorry
+
+end SLang
