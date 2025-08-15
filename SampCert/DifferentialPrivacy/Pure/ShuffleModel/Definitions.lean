c3a241a403b60044464615cd1f429ad849d0f0c4
Renee Tetlow <tetlow01@allegheny.edu>
2025-08-15 18:55:21 -0400

    added defs

--- SampCert/DifferentialPrivacy/Pure/ShuffleModel/Definitions.lean
+++ SampCert/DifferentialPrivacy/Pure/ShuffleModel/Definitions.lean
@@ -19,12 +19,62 @@ match l with
     let rest : List α ← Shuffler tl
     return rest.insertNth i hd
 
-
   /- This is the Shuffle Model. -/
 def RRShuffle(query: T -> Bool) (num : Nat) (den : PNat) (h: 2 * num < den)(l: List T) := do
   let l ← RandomizedResponse.RRSample query num den h l
   let b ← Shuffler l
   return b
+
+#check Shuffler
+#check RandomizedResponse.RRSample
+#check Mechanism
+
+def UniformShuffler {U: Type}[BEq U](f: List U → SLang (List U)) : Prop :=
+  ∀ l₁ l₂: List U, f l₁ l₂ = if List.isPerm l₁ l₂ then (1: ENNReal)/(l₁.length.factorial) else (0: ENNReal)
+
+lemma UniformShuffler_norms {U: Type}[DecidableEq U][BEq U](f: List U → SLang (List U)) (h:UniformShuffler f → True)
+:∀(b: List U),∑' (i : List U), f b i = 1 := by
+  intro b
+  have h1 : ∑' (i : List U), f b i = ∑' (i: List U), if List.isPerm b i then f b i else f b i := by aesop
+  rw [h1]
+  have h2 (x i : List U) : f x i  = if List.isPerm x i then  (1: ENNReal)/(x.length.factorial) else (0: ENNReal) := by
+    sorry
+  conv =>
+    enter [1,1,i]
+    rw [h2]
+    simp
+  sorry
+
+
+
+
+def ShuffleAlgorithm [BEq U](m : List T → SLang (List U))(f : List U → SLang (List U))(_: UniformShuffler f → True)(l: List T) := do
+  let x ← m l
+  let b ← f x
+  return b
+
+lemma ShuffleAlgorithm_norms {U: Type} [BEq U] (m : Mechanism T (List U))(f : List U → SLang (List U))(h: UniformShuffler f → True)(l: List T):
+HasSum (ShuffleAlgorithm (fun x => (m x).1) f h l) 1  := by
+  unfold ShuffleAlgorithm
+  simp_all only [bind, pure, bind_pure]
+  unfold probBind
+  simp [Summable.hasSum_iff ENNReal.summable]
+  rw [ENNReal.tsum_comm]
+  conv =>
+    enter [1,1,b]
+    rw [ENNReal.tsum_mul_left]
+    enter [2]
+    apply UniformShuffler_norms
+    exact h
+  simp
+  rw [← Summable.hasSum_iff ENNReal.summable]
+  exact (m l).2
+
+def ShuffleAlgorithm_PMF {U: Type}[BEq U] (m : Mechanism T (List U ))(f : List U → SLang (List U))(h: UniformShuffler f → True)(l: List T) : PMF (List U) :=
+  ⟨ShuffleAlgorithm (fun x => (m x).1) f h l, ShuffleAlgorithm_norms m f h l⟩
+
+theorem ShuffleAlgorithm_is_DP [BEq U](m : Mechanism T (List U))(f : List U → SLang (List U))(ε : ℝ)(hdp: DP_withUpdateNeighbour m ε)
+(hsa: UniformShuffler f → True): DP_withUpdateNeighbour (ShuffleAlgorithm_PMF m f hsa) ε := by sorry
 
 def BinomialSample (seed: MultiBernoulli.SeedType)(n:PNat) := do
   let seeds := List.replicate n seed
